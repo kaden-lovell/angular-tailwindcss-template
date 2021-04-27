502cf16a30af94f5fd91a7a03f6a22f552c80e87
= <kadenlovell@live.com>
2021-04-27 00:45:24 -0600

    added chart.js, and created demo graph cards

--- client/src/app/components/graph/graph.component.ts
+++ client/src/app/components/graph/graph.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
-  styleUrls: ['./graph.component.scss']
+  styleUrls: ['./graph.component.scss'],
 })
-export class GraphComponent implements OnInit {
+export class GraphComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvas')
+  canvas: ElementRef;
 
-  constructor() { }
+  @ViewChild('canvas2')
+  canvas2: ElementRef;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    const myChartRef1 = this.canvas.nativeElement;
+    const myChartRef2 = this.canvas2.nativeElement;
+    new Chart(myChartRef1, {
+      type: 'bar',
+      data: {
+        //Bring in data
+        labels: ['Jan', 'Feb', 'March'],
+        datasets: [
+          {
+            label: 'chart 1',
+            data: [86, 67, 91],
+            borderColor: ['#DC2626', '#059669', '#2563EB'],
+            backgroundColor: ['#DC2626', '#059669', '#2563EB'],
+            color: ['#DC2626', '#059669', '#2563EB'],
+          },
+        ],
+      },
+      options: {
+        //Customize chart options
+      },
+    });
+
+    new Chart(myChartRef2, {
+      type: 'line',
+      data: {
+        //Bring in data
+        labels: ['Jan', 'Feb', 'March', 'april', 'may'],
+        datasets: [
+          {
+            label: 'chart 2',
+            data: [44, 67, 91, 499, 329],
+            borderColor: ['#DC2626', '#059669', '#2563EB'],
+            color: ['#DC2626', '#059669', '#2563EB'],
+          },
+        ],
+      },
+      options: {
+        //Customize chart options
+      },
+    });
   }
-
 }
